0a49cbdb33e1742ba5d581e0d7fb0ac1a6e0326a
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2023-10-30 20:55:37 -0400

    add streamlit material

---
+++ lessons/11_streamlitAndSqlite/sandbox/streamlit-dbms.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+import streamlit as st
+import pandas as pd
+import numpy as np
+import sqlite3
+
+def main():
+	"""driver function of program"""
+	st.title('My Database App')
+# end of main()
+
+main()
+
